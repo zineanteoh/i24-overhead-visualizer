e069a4ee8ab2e498282a3a9cca02466a18b170ad
Zi Nean Teoh <zinean00@gmail.com>
2022-09-19 13:34:22 -0500

    successfully deployed openCV plots to Flask on web browser

--- overhead_compare_v2.py
+++ overhead_compare_v2.py
@@ -12,9 +12,18 @@ TODOs:
 """
 
 from i24_database_api import DBClient
+from datetime import datetime
+from flask import Response
+from flask import Flask
+from flask import render_template
+import numpy as np
+import threading
+import time
 import json
 import cv2
-import numpy as np
+
+outputFrame = None
+lock = threading.Lock()
 
 class OverheadCompareV2():
     """
@@ -88,7 +97,9 @@ class OverheadCompareV2():
         # add line 
         
 
-    def animate(self):
+    def animate(self, save, upload, stream, extra): 
+        
+        # initiate frame 
         self.refresh_frame()
         
         x1 = 0
@@ -98,17 +109,21 @@ class OverheadCompareV2():
         h = 25
         c = (255, 0, 0)
         
-        # write to file
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter('your_video.mp4', fourcc, self.framerate, (self.window_w, self.window_h))
+        if save:
+            now = datetime.utcfromtimestamp(int(time.time())).strftime('%Y-%m-%d_%H-%M-%S')
+            file_name = now+"_" + "random-trajectory" +extra+".mp4"
+            path_name = "/home/zitest/Desktop/i24-overhead-visualizer/videos/" + file_name
+            # write to file
+            fourcc = cv2.VideoWriter_fourcc(*'MPEG')
+            out = cv2.VideoWriter(path_name, fourcc, self.framerate, (self.window_w, self.window_h))
 
         end = False
         frame = 0
+        if stream:
+            global outputFrame, lock
         while True:
             if end:
                 break
-            
-            
             
             # temporary move mechanism
             if x1 + w > self.window_w:
@@ -128,10 +143,12 @@ class OverheadCompareV2():
             cv2.putText(self.frame, 'Frame # {f}'.format(f=frame), 
                         org=(10, self.window_h - 20), fontFace=cv2.FONT_HERSHEY_DUPLEX,
                         fontScale=1, color=(0, 0, 0), thickness=1, lineType=1)
-            cv2.imshow("i24 overhead compare v2", self.frame)
+            if not stream:
+                cv2.imshow("i24 overhead compare v2", self.frame)
             
             # save
-            out.write(self.frame.astype(np.uint8))
+            if save:
+                out.write(self.frame.astype(np.uint8))
             
             # end with escape
             k = cv2.waitKey(int(1000/self.framerate)) & 0xFF
@@ -140,32 +157,90 @@ class OverheadCompareV2():
                 break
             
             frame += 1
-        print("fr: ", int(1000/self.framerate))
-        out.release()
+            
+            # acquire lock, set output frame, and release lock
+            if stream:
+                print('Current lock: ', lock)
+                with lock:
+                    outputFrame = self.frame.copy()
+        
+        if save:
+            out.release()
         cv2.destroyAllWindows()
         return
+    
+    def generate_stream(self):
+        # grab global references to the output frame and lock variables
+    	global outputFrame, lock
+    	# loop over frames from the output stream
+    	while True:
+    		# wait until the lock is acquired
+    		with lock:
+    			# check if the output frame is available, otherwise skip
+    			# the iteration of the loop
+    			if outputFrame is None:
+    				continue
+    			# encode the frame in JPEG format
+    			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+    			# ensure the frame was successfully encoded
+    			if not flag:
+    				continue
+    		# yield the output frame in the byte format
+    		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+    			bytearray(encodedImage) + b'\r\n')
+    
+    def setup_stream(self):
+        self.app = Flask(__name__)
+        
+        @self.app.route("/")
+        def index():
+        	# return the rendered template
+        	return render_template("index.html")
+        
+        @self.app.route("/video_feed")
+        def video_feed():
+        	# return the response generated along with the specific media
+        	# type (mime type)
+        	return Response(self.generate_stream(),
+        		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+    def start_stream(self):
+        self.app.run(host="0.0.0.0",
+                     port=8000,
+                     debug=True,
+                     threaded=True,
+                     use_reloader=False)
 
 
 def main(rec, gt = "groundtruth_scene_2_57", framerate = 25, x_min=-100, x_max=2200, offset=0, duration=500, 
-         save=False, upload=False, extra=""):
+         save=False, upload=False, extra="", stream=False):
     
     # change path to config
     with open("/home/zitest/Desktop/i24-overhead-visualizer/config.json") as f:
         db_param = json.load(f)
-    
-    print(db_param)
     
     raw = rec.split("__")[0]
     print("Generating a video for {}...".format(rec))
     p = OverheadCompareV2(db_param, 
                 collections = [gt, raw, rec],
                 framerate = framerate, x_min = x_min, x_max=x_max, offset = offset, duration=duration)
-    # p.animate(save=save, upload=upload, extra=extra)
-    p.animate()
+    if stream:
+        p.setup_stream()
+        
+        t = threading.Thread(target=p.animate, args=(
+            False, False, True, "",))
+        t.daemon = True
+        t.start()
+        
+        print("starting animate process: ", t.native_id)
+        
+        p.start_stream()
+    else:
+        p.animate(save=save, upload=upload, stream=stream, extra=extra)
     
 if __name__=="__main__":
 
-    main(rec = "zonked_cnidarian--RAW_GT2__articulates", save=False, upload = False)
+    main(rec = "zonked_cnidarian--RAW_GT2__articulates", save=False, upload=False, stream=True)
     
         
 
